b47f844b83ad4e9e408d5a19faa5e07b04a2cdb0
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-02-09 10:01:47 +0100

    Right directory

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require('controllers/post.php');
+
+if(isset($_GET['action'])) {
+    if($_GET['action'] == 'listPosts'){
+        listPost();
+    }
+    elseif ($_GET['action'] == 'post'){
+        if(isset($_GET['id']) && $_GET['id'] > 0){
+            post();
+        }else{
+            echo 'Error: no id has been sent';
+        }
+    }
+}else{
+    listUsers();
+   // header('Location: http://localhost:8888/Blog/views/startbootstrap-freelancer-gh-pages/');
+}
+
+$req = getPosts();
+
+
