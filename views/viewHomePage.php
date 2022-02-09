9e480dba9952f87fcf46c268e411e3fbb4631fe0
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-02-09 16:03:10 +0100

    Contact form with mail notification

--- views/viewHomePage.php
+++ views/viewHomePage.php
@@ -76,6 +76,11 @@ $favicon = 'views/startbootstrap-freelancer-gh-pages/assets/favicon.ico';
         <!-- Masthead Heading-->
         <h1 class="masthead-heading text-uppercase mb-0"> <?= $firstname . ' ' . $name ?> </h1>
 
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+        </div>
+
         <!-- Masthead Subheading-->
         <p class="masthead-subheading font-weight-light mb-0">
             <?= $bio ?>
@@ -159,10 +164,10 @@ $favicon = 'views/startbootstrap-freelancer-gh-pages/assets/favicon.ico';
     </div>
 </section>
 <!-- About Section-->
-<section class="page-section bg-primary text-white mb-0" id="about">
+<section class="page-section bg-secondary text-white mb-0" id="about">
     <div class="container">
         <!-- About Section Heading-->
-        <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+        <h2 class="page-section-heading text-center text-uppercase text-white">À propos</h2>
         <!-- Icon Divider-->
         <div class="divider-custom divider-light">
             <div class="divider-custom-line"></div>
@@ -171,8 +176,9 @@ $favicon = 'views/startbootstrap-freelancer-gh-pages/assets/favicon.ico';
         </div>
         <!-- About Section Content-->
         <div class="row">
-            <div class="col-lg-4 ms-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
-            <div class="col-lg-4 me-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+            <div class="col-lg-4 ms-auto"><p class="lead">Après un bachelier en e-business, j'ai décidé de me spécialiser et de me consacrer à 100% à la programmation.
+                    J'aime avoir la possibilité de développer une idée en partant de zéro ou d'aider les entreprises à développer leur projet.</p></div>
+            <div class="col-lg-4 me-auto"><p class="lead">Aider les entreprises à améliorer leurs processus grâce à des applications, rendre une entreprise visible sur le web, améliorer des sites existants, sont des exemples d'interventions que je suis ravie de pouvoir faire.</p></div>
         </div>
         <!-- About Section Button-->
         <div class="text-center mt-4">
@@ -183,77 +189,111 @@ $favicon = 'views/startbootstrap-freelancer-gh-pages/assets/favicon.ico';
         </div>
     </div>
 </section>
+
+<!-- Mail -->
+<?php
+
+if(isset($_POST['mailform']))
+{
+    if((!empty($_POST['name']) AND (!empty($_POST['email']) AND (!empty($_POST['message']))))){
+
+        $header="MIME-Version: 1.0\r\n";
+        $header.='From:"Guillaume De Backre"<guillaume@gmail.com>'."\n";
+        $header.='Content-Type:text/html; charset="uft-8"'."\n";
+        $header.='Content-Transfer-Encoding: 8bit';
+
+        $message='
+
+<html>
+
+    <body>
+
+        <div align="center">
+          <p>Prénom : '.$_POST['firstname'].'</p>  <br />
+          <p>Nom : '.$_POST['name'].' </p> <br />
+          <p>Message :</p> <br />'.nl2br($_POST['message']).' </p> <br />
+          <p>Email :  '.$_POST['email'].'  <br />
+
+        </div>
+
+    </body>
+
+</html>
+
+';
+
+        mail("debackre.guillaume@gmail.com", "CONTACT - GUIBLOG.be", $message, $header);
+        $msg ="Votre message a bien été envoyé !";
+
+            }
+    else{
+        $msg = 'Tous les champs doivent être remplis';
+    }
+
+}
+
+?>
+
 <!-- Contact Section-->
 <section class="page-section" id="contact">
     <div class="container">
         <!-- Contact Section Heading-->
-        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contactez-moi</h2>
         <!-- Icon Divider-->
         <div class="divider-custom">
             <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-            <div class="divider-custom-line"></div>
+
         </div>
         <!-- Contact Section Form-->
         <div class="row justify-content-center">
             <div class="col-lg-8 col-xl-7">
                 <!-- * * * * * * * * * * * * * * *-->
                 <!-- * * SB Forms Contact Form * *-->
-                <!-- * * * * * * * * * * * * * * *-->
-                <!-- This form is pre-integrated with SB Forms.-->
-                <!-- To make this form functional, sign up at-->
-                <!-- https://startbootstrap.com/solution/contact-forms-->
-                <!-- to get an API token!-->
-                <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                <p>Veuillez renseigner vos coordonnées et taper votre message.</p>
+                <form method="POST" action="">
+                    <!-- Firstname input-->
+                    <div class="form-floating mb-3">
+                        <input class="form-control" type="text" name="firstname" placeholder="Votre prénom" />
+                        <label for="name">Votre prénom</label>
+                        <div class="invalid-feedback" data-sb-feedback="name:required">Un prénom doit être renseigné</div>
+                    </div>
                     <!-- Name input-->
                     <div class="form-floating mb-3">
-                        <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
-                        <label for="name">Full name</label>
-                        <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                        <input class="form-control" type="text" name="name" placeholder="Votre nom" />
+                        <label for="name">Votre nom</label>
+                        <div class="invalid-feedback" data-sb-feedback="name:required">Un nom doit être renseigné</div>
                     </div>
                     <!-- Email address input-->
                     <div class="form-floating mb-3">
-                        <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
+                        <input class="form-control" type="email" name="email" placeholder="Votre email" />
                         <label for="email">Email address</label>
                         <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
                         <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
                     </div>
-                    <!-- Phone number input-->
-                    <div class="form-floating mb-3">
-                        <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
-                        <label for="phone">Phone number</label>
-                        <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                    </div>
+                    <!-- Textarea-->
                     <!-- Message input-->
                     <div class="form-floating mb-3">
-                        <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
+                        <textarea class="form-control" name="message" placeholder="Votre message" ></textarea>
                         <label for="message">Message</label>
-                        <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                        <div class="invalid-feedback" data-sb-feedback="message:required">Un message est nécessaire.</div>
                     </div>
-                    <!-- Submit success message-->
-                    <!---->
-                    <!-- This is what your users will see when the form-->
-                    <!-- has successfully submitted-->
-                    <div class="d-none" id="submitSuccessMessage">
-                        <div class="text-center mb-3">
-                            <div class="fw-bolder">Form submission successful!</div>
-                            To activate this form, sign up at
-                            <br />
-                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                        </div>
-                    </div>
-                    <!-- Submit error message-->
-                    <!---->
-                    <!-- This is what your users will see when there is-->
-                    <!-- an error submitting the form-->
-                    <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
                     <!-- Submit Button-->
-                    <button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Send</button>
+                    <button class="btn btn-primary btn-xl" type="submit" value="Envoyer" name="mailform">Envoyer</button>
+
+                    <!-- message display -->
+                    <?php
+                    if(isset($msg)){
+                        echo $msg;
+                    }
+                    ?>
+
                 </form>
             </div>
         </div>
     </div>
 </section>
+
+
 <!-- Footer-->
 <footer class="footer text-center">
     <div class="container">
