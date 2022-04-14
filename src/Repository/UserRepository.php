05ccce49c943c7f04b7fe5eff116e8a2b46dac4c
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-04-14 10:29:30 +0200

    modifyUser method

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -11,13 +11,28 @@ class UserRepository extends Repository
         parent::__construct($dbConnection, 'user', User::class);
     }
 
-    //INSERT NEW ELEMENT
+    //CREATE NEW USER
     public function createUser($firstname, $name, $email, $status, $bio, $password)
     {
          $insertInto = $this->dbConnection->getPDO()->prepare('INSERT INTO user (firstname, name, email, status, bio, password) VALUES (?,?,?,?,?,?)');
          return $insertInto ->execute(array($firstname, $name, $email, $status, $bio, $password));
     }
-
-
-
+    //MODIFY ONE USER
+    public function modifyUser($id, $firstname, $name, $email, $bio, $password)
+    {
+        $modifyUser = $this->dbConnection->getPDO()->prepare('UPDATE user SET firstname = :firstname, name = :name, email = :email, bio = :bio, password = :password WHERE id = :id');
+            if (
+                $modifyUser->execute([
+                'firstname' => $firstname,
+                'name' => $name,
+                'email' => $email,
+                'bio' => $bio,
+                'password' => $password,
+                'id' => $id])
+            ){
+                return 'User successfully modified';
+            }else{
+                return 'An error has occured';
+            }
+    }
 }
