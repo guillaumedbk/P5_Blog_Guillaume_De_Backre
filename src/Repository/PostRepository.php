7b712b81f776570850e3bbc148cfb4fda3006b5f
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-04-14 15:27:09 +0200

    Method to modify one Post

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -19,4 +19,20 @@ class PostRepository extends Repository
         $insertInto = $this->dbConnection->getPDO()->prepare('INSERT INTO post (userId, title, chapo, content, lastUpdate) VALUES (?,?,?,?,NOW())');
         return $insertInto ->execute(array($post->getUserId(), $post->getTitle(), $post->getChapo(), $post->getContent()));
     }
+    //MODIFY ONE POST
+    public function modifyPost(Post $post, $id)
+    {
+        $modifyPost = $this->dbConnection->getPDO()->prepare('UPDATE post SET title = :title, chapo = :chapo, content = :content, lastUpdate = NOW() WHERE id = :id');
+        if(
+            $modifyPost -> execute([
+                'title' => $post->getTitle(),
+                'chapo' => $post->getChapo(),
+                'content' => $post->getContent(),
+                'id' => $id])
+        ) {
+            return 'Post successfully modified';
+        }else{
+            return 'An error has occured';
+        }
+    }
 }
