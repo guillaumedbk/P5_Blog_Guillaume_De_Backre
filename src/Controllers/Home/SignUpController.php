938c46d4fae9ca831861f880ba0eb13a99fe202a
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-04-26 14:42:48 +0200

    Creation of methods to check data validity
    validate(), checkEmailValidity(), checkStatusValidity() methods and use them to check the datas in the postSignUpController() before sending the $user object to the UserRepository

--- src/Controllers/Home/SignUpController.php
+++ src/Controllers/Home/SignUpController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Home;
 
 use App\Controllers\Controller;
 use App\Entity\User\User;
+use App\Repository\FileLogger;
 use App\Repository\UserRepository;
 use App\Router\Request;
 
@@ -27,9 +28,24 @@ class SignUpController extends Controller
 
     public function postSignUpController(): void
     {
-        //$user = new User($_POST['firstname'], $_POST['name'], $_POST['email'], 'utilisateur', $_POST['bio'], $_POST['password']);
-        // $userrepo = new UserRepository($this->getDBConnexion());
-        //  $insert = $userrepo->createUser($user);
-        echo 'User creation';
+        $user = new User($_POST['firstname'], $_POST['name'], $_POST['email'], 'visitor', $_POST['bio'], $_POST['password']);
+        try {
+            //CHECK DATA VALIDITY OF firstname and name
+            $validation = array($user->getFirstname(), $user->getName());
+            foreach ($validation as $value) {
+                $this->validate($value);
+            }
+            //CHECK EMAIL VALIDITY
+            $this->checkEmailValidity($user->getEmail());
+            //CHECK STATUS VALIDITY
+            $this->checkStatusValidity($user->getStatus());
+            //INSERT NEW USER
+            $userrepo = new UserRepository($this->getDBConnexion());
+            $insert = $userrepo->createUser($user);
+        } catch (\Exception $exception) {
+            $logger = new FileLogger('logger.log');
+            $logger->critical("The following error has occured: {$exception->getMessage()} at line: {$exception->getLine()} in file {$exception->getFile()}");
+            throw new \Exception("The following error has occured:  {$exception->getMessage()} at line: {$exception->getLine()} in file {$exception->getFile()}");
+        }
     }
 }
