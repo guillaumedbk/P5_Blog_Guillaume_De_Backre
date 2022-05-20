64ce28e838b863f05fea1647c285c5a9bb461286
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-05-20 16:31:05 +0200

    Data validation for the contact method

--- src/Controllers/Home/ContactController.php
+++ src/Controllers/Home/ContactController.php
@@ -3,26 +3,44 @@
 namespace App\Controllers\Home;
 
 use App\Controllers\Controller;
+use App\Repository\UserRepository;
 use App\Router\Request;
 use App\Validator\Security\SecurePostData;
+use App\Validator\Validators\ContactAssertMapValidator;
+use App\Validator\Validators\Validator;
 
 class ContactController extends Controller
 {
+    private $checkErrors;
+
     public function __invoke(Request $request): void
     {
         if (!empty($request->getData())) {
+            //retrieve data in a secure way
             $security = new SecurePostData();
             $securedData = $security->secureData($request->getData());
+            //data validation
+            $repo = new UserRepository($this->getDBConnexion());
+            //array to object
+            $securedData = $repo->hydrate($securedData);
+            $validator = new Validator();
+            $contactValidator = new ContactAssertMapValidator();
+            $this->checkErrors = $validator->validate($contactValidator, $securedData);
 
+            //send email
             $to = 'debackre.guillaume@gmail.com';
             $subject = 'the subject';
-            $message = $securedData['message'];
+            $message = $securedData->message;
             $headers = array(
                 'From' => 'debackre.guillaume@gmail.com',
                 'Reply-To' => 'debackre.guillaume@gmail.com',
                 'X-Mailer' => 'PHP/' . phpversion()
             );
-            var_dump(mail($to, $subject, $message, $headers));
+            if(mail($to, $subject, $message, $headers) === true){
+                echo  'votre mail a bien été envoyé';
+            }else{
+                 echo "une erreur est survenue lors de l'envoie du mail";
+            }
         }
     }
 }
---
+++ src/Validator/Validators/ContactAssertMapValidator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Validator\Validators;
+
+use App\Validator\AssertMapValidatorInterface;
+use App\Validator\Asserts\AssertEmail;
+use App\Validator\Asserts\AssertEmailUniquess;
+use App\Validator\Asserts\AssertLength;
+use App\Validator\Asserts\AssertNotBlank;
+
+class ContactAssertMapValidator implements AssertMapValidatorInterface
+{
+    private array $asserts;
+
+    public function __construct()
+    {
+        $this->asserts = [
+            'firstname' => [
+                new AssertNotBlank(),
+                new AssertLength(3, 255)
+            ],
+            'name' => [
+                new AssertNotBlank(),
+                new AssertLength(3, 255),
+            ],
+            'email' => [
+                new AssertNotBlank(),
+                new AssertLength(5, 40),
+                new AssertEmail(),
+            ],
+            'message' => [
+                new AssertNotBlank(),
+                new AssertLength(4, 60),
+            ],
+        ];
+    }
+
+    public function getAsserts(): array
+    {
+        return $this->asserts;
+    }
+}
