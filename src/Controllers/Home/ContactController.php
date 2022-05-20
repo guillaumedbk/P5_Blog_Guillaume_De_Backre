2acfda72784c4d574ee6f4856862a889015bbf4d
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-05-20 14:42:01 +0200

    ContactController to send email for contact form

--- src/Controllers/Home/ContactController.php
+++ src/Controllers/Home/ContactController.php
@@ -4,18 +4,22 @@ namespace App\Controllers\Home;
 
 use App\Controllers\Controller;
 use App\Router\Request;
+use App\Validator\Security\SecurePostData;
 
 class ContactController extends Controller
 {
     public function __invoke(Request $request): void
     {
         if (!empty($request->getData())) {
+            $security = new SecurePostData();
+            $securedData = $security->secureData($request->getData());
+
             $to = 'debackre.guillaume@gmail.com';
             $subject = 'the subject';
-            $message = 'hello';
+            $message = $securedData['message'];
             $headers = array(
-                'From' => 'webmaster@example.com',
-                'Reply-To' => 'webmaster@example.com',
+                'From' => 'debackre.guillaume@gmail.com',
+                'Reply-To' => 'debackre.guillaume@gmail.com',
                 'X-Mailer' => 'PHP/' . phpversion()
             );
             var_dump(mail($to, $subject, $message, $headers));
